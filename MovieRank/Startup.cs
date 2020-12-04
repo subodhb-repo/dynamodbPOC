a485bbb3a85f3b88f2d8213a5e5b5eabf0744bf6
Subodh Bhandary <subodhb@outlook.com.au>
2020-12-04 13:55:06 +1100

    initial setup

---
+++ MovieRank/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.Extensions.NETCore.Setup;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MovieRank
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAWSService<IAmazonDynamoDB>();
+            services.AddDefaultAWSOptions(
+                new AWSOptions
+                {
+                    Region = RegionEndpoint.GetBySystemName("ap-southeast-2")
+                });
+            services.AddSingleton<>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ MovieRanks.Libs/Repositories/MovieRankRepository.cs
@@ -0,0 +1,84 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using MovieRank.Contracts;
+using MovieRanks.Libs.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MovieRanks.Libs.Repositories
+{
+    public class MovieRankRepository
+    {
+        private readonly DynamoDBContext _context;
+
+        public MovieRankRepository(IAmazonDynamoDB dynamoDBClient)
+        {
+            _context = new DynamoDBContext(dynamoDBClient);
+        }
+
+        /// <summary>
+        /// Scan
+        /// </summary>
+        public async Task<IEnumerable<MovieDb>> GetAllItems()
+        {
+            return await _context.ScanAsync<MovieDb>(new List<ScanCondition>()).GetRemainingAsync();
+        }
+
+        /// <summary>
+        /// Load
+        /// </summary>
+        public async Task<MovieDb> GetMovie(int userID, string movieName)
+        {
+            return await _context.LoadAsync<MovieDb>(userID, movieName);
+        }
+
+        /// <summary>
+        /// Query filter
+        /// </summary>
+        public async Task<IEnumerable<MovieDb>> GetUsersRankedMoviesByMovieTitle(int userID, string movieName)
+        {
+            var config = new DynamoDBOperationConfig
+            {
+                QueryFilter = new List<ScanCondition>
+                {
+                    new ScanCondition("MovieName", ScanOperator.BeginsWith, movieName)
+                }
+            };
+
+            return await _context.QueryAsync<MovieDb>(userID, config).GetRemainingAsync();
+        }
+
+        /// <summary>
+        /// Add new entity
+        /// </summary>
+        public async Task AddMovie(MovieDb movie)
+        {
+            await _context.SaveAsync<MovieDb>(movie);
+        }
+
+        /// <summary>
+        /// Add new entity
+        /// </summary>
+        public async Task UpdateMovie(int userID, MovieUpdateRequest newDetails)
+        {
+            var movie = await GetMovie(userID, newDetails.MovieName);
+            movie.Ranking = newDetails.Ranking;
+            await _context.SaveAsync<MovieDb>(movie);
+        }
+
+        /// <summary>
+        /// Using Secondary index
+        /// </summary>
+        public async Task<IEnumerable<MovieDb>> GetMovieRank(string movieName)
+        {
+            var config = new DynamoDBOperationConfig
+            {
+                IndexName = "MovieName-index",
+                
+            };
+
+            return await _context.QueryAsync<MovieDb>(movieName, config).GetRemainingAsync();
+        }
+    }
+}
